60d70c33ae9ae774752408db48e5b5b2be246beb
ArovaSyams <wifqoarova17@gmail.com>
2021-01-15 14:35:56 +0700

    Menambahkan tampilan fitur login khusus untuk admin

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -11,4 +11,17 @@
             <a href=""><i class="bi bi-whatsapp media"></i></a>
         </div>
     </div>
+    <div class="login">
+        <form>
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="email" class="form-control" id="username" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 </footer>
