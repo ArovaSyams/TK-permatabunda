9465250dd708ebed68409910506fe411694b7938
ArovaSyams <wifqoarova17@gmail.com>
2021-01-18 16:35:40 +0700

    Menambah halaman 'Berita' beserta isinya

--- app/Views/pages/berita.php
+++ app/Views/pages/berita.php
@@ -1,5 +1,53 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
+<div class="head"></div>
+<div class="berita">
+    <div class="card card-berita mb-3" style="max-width: 100%;">
+      <div class="row g-0">
+        <div class="img-berita col-md-4">
+          <img src="/img/frozen2-bg.jpg" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body card-bodies">
+            <h5 class="card-title">PENDAFTARAN PESERTA DIDIK BARU TELAH DIBUKA</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <a href="" class="btn btn-success">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card card-berita mb-3" style="max-width: 100%;">
+      <div class="row g-0">
+        <div class="img-berita col-md-4">
+          <img src="/img/frozen2-bg.jpg" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">PENDAFTARAN PESERTA DIDIK BARU TELAH DIBUKA</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <a href="" class="btn btn-success">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card card-berita mb-3" style="max-width: 100%;">
+      <div class="row g-0">
+        <div class="img-berita col-md-4">
+          <img src="/img/frozen2-bg.jpg" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">PENDAFTARAN PESERTA DIDIK BARU TELAH DIBUKA</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <a href="" class="btn btn-success">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
 
 <?= $this->endSection(); ?>
