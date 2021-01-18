bf8d400d52585f875cf3e83f90be3bcd02000b6f
ArovaSyams <wifqoarova17@gmail.com>
2021-01-18 16:45:03 +0700

    Memfixan renponsive halaman 'Berita'

--- app/Views/pages/berita.php
+++ app/Views/pages/berita.php
@@ -9,7 +9,7 @@
           <img src="/img/frozen2-bg.jpg" alt="...">
         </div>
         <div class="col-md-8">
-          <div class="card-body card-bodies">
+          <div class="card-body">
             <h5 class="card-title">PENDAFTARAN PESERTA DIDIK BARU TELAH DIBUKA</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
--- app/Views/pages/hubungi.php
+++ app/Views/pages/hubungi.php
@@ -1,5 +1,8 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-
+<div class="head"></div>
+<div class="hubungi">
+    
+</div>
 <?= $this->endSection(); ?>
